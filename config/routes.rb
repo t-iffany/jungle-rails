9f07c3cdcd42b2a86bf52bbd22b6a3b039cfca76
t-iffany <tiffanyxleong@gmail.com>
2023-01-25 17:28:57 +0000

    Implement About page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
