b4960d51dec3e1661255709444db62d939a9fbce
t-iffany <tiffanyxleong@gmail.com>
2023-01-26 09:17:47 +0000

    Update application_controller with methods to look up user, set current_user, and authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
   protect_from_forgery with: :exception
+
+  # methods to look up user
+  # if logged in, save their user object to a variable called @current_user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  # helper_method allows us to use @current_user in our view files
+  helper_method :current_user
+
+  # if someone is not logged in, they are not authorized to see certain pages, redirect to login page
+  def authorize
+    redirect_to '/login' unless current_user
+  end
 
   private
 
