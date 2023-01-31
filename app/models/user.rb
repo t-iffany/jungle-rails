c190761b0b351f19d6a9596b8205e6b143dd08f5
t-iffany <tiffanyxleong@gmail.com>
2023-01-31 08:15:49 +0000

    -Setup User model spec file -Define validation specs -Add validation for pw length -Add authenticate_with_credentials -Add tests for edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
 
   has_secure_password
-  
+
+  validates :password, presence: true, length: { minimum: 3}
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  before_save { self.email.downcase! }
+
+  def self.authenticate_with_credentials(email, password)
+    User.find_by_email(email.strip.downcase)&.authenticate(password)
+  end
+
 end
